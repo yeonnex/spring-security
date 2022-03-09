14f738453ce07e0def2f128513b4b0fa7fc13d3a
yeonnex <82166132+yeonnex@users.noreply.github.com>
2022-03-09 23:30:24 +0900

    pom.xml 에 jwt 라이브러리 의존성 추가와 access token 과 refresh token 생성 - 두 토큰 생성 모두 로그인이 성공적으로 이루어졌을때 동작하는 함수인 successfulAuthentication 함수에서 진행 - access token 의 만료 시간은 10분으로 설정 - refresh token 의 만료 시간은 30분으로 설정 - response.setHeader("access_token", access_token); - response.setHeader("refresh_token", refresh_token); - 이제 유저가 로그인을 성공적으로 했다면, 응답 헤더에 두개의 토큰을 받을 수 있게 된다!

--- userservice/src/main/java/com/example/userservice/filter/CustomAuthenticationFilter.java
+++ userservice/src/main/java/com/example/userservice/filter/CustomAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.example.userservice.filter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.sun.xml.bind.v2.TODO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -7,6 +9,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -14,6 +18,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 /**
  * Whenever the user tries to login
@@ -45,10 +51,27 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         return authenticationManager.authenticate(authenticationToken);
     }
 
-    // 위 함수를 통해 인증이 성공하면, 이 함수가 실행되게 된다다
+    // 위 함수를 통해 인증이 성공하면, 이 함수가 실행되게 된다
+    // 여기서 access token 과 refresh 토큰 생성 예정
    @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+        // org.springframework.security.core.userdetails.User
+        User user = (User)authentication.getPrincipal(); // Object 를 반환하기 때문. 이 로그인 성공한 유저 정보로 json 웹토큰을 만들어보자
+       Algorithm algorithm = Algorithm.HMAC256("secret".getBytes()); // TODO secret 값 .yml 파일에 저장하기
+       String access_token = JWT.create()
+               .withSubject(user.getUsername())
+               .withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 60 * 10)) // 토큰의 만료시간은 10분
+               .withIssuer(request.getRequestURL().toString())
+               .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+               .sign(algorithm);
+       String refresh_token = JWT.create()
+               .withSubject(user.getUsername())
+               .withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 60 * 30)) // 토큰의 만료시간은 10분
+               .withIssuer(request.getRequestURL().toString())
+//               .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList())) // refresh 토큰의 경우 권한값을 안줘도 괜찮음
+               .sign(algorithm);
+       response.setHeader("access_token", access_token);
+       response.setHeader("refresh_token", refresh_token);
     }
 
     /**
